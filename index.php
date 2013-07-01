e5db35030b3acf6b076eca3101e2e767f8edb4ea
JT Grauke <jtgrauke@gmail.com>
2013-07-01 07:11:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,57 +8,45 @@
     <link rel="shortcut icon" href="images/favicon.png">
 </head>
 <body>
-    <div class="wrapper">
-        <div class="top-nav nav-wrapper">
-            <nav class="nav">
-                <ul>
-                    <li><a href="#">&plus;You</a></li>
-                    <li><a class="selected" href="#">Search</a></li>
-                    <li><a href="#">Images</a></li>
-                    <li><a href="#">Maps</a></li>
-                    <li><a href="#">Play</a></li>
-                    <li><a href="#">YouTube</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Gmail</a></li>
-                    <li><a href="#">Drive</a></li>
-                    <li><a href="#">Calendar</a></li>
-                    <li>
-                        <span class="more">
-                            <a href="#">More</a>
-                            <span class="triangle"></span>
-                        </span>
-                    </li>
-                </ul>
-            </nav>
-        </div><!--.nav-wrapper-->
+    <nav class="top-nav">
+        <ul>
+            <li><a href="#">&plus;You</a></li>
+            <li><a class="selected" href="#">Search</a></li>
+            <li><a href="#">Images</a></li>
+            <li><a href="#">Maps</a></li>
+            <li><a href="#">Play</a></li>
+            <li><a href="#">YouTube</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Gmail</a></li>
+            <li><a href="#">Drive</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li class="more"><a href="#">More</a><span class="triangle"></span></li>
+        </ul>
+    </nav>
 
-        <a href="#" class="button red">Sign In</a>
+    <a href="#" class="button red">Sign In</a>
 
-        <div class="search-wrapper">
-            <div class="search">
-                <img class="logo" alt="Google" src="images/google-logo.png">
-                <div class="input">
-                    <input>
-                    <a title="Search by voice" class="tooltip"><span class="mic"></span></a>
-                </div>
-                <div class="button-wrapper">
-                    <a href="#" class="button grey">Google Search</a>
-                    <a href="#" class="button grey">I'm Feeling Lucky</a>
-                </div>
-            </div><!--.search-->
-        </div><!--.search-wrapper-->
+    <img class="logo" alt="Google" src="images/google-logo.png">
 
-        <div class="bottom-nav nav-wrapper">
-            <nav class="nav">
-                <ul>
-                    <li><a href="#">Advertising Programs</a></li>
-                    <li><a href="#">Business Solutions</a></li>
-                    <li><a href="#">Privacy &amp; Terms</a></li>
-                    <li><a href="#">&plus;Google</a></li>
-                    <li><a href="#">About Google</a></li>
-                </ul>
-            </nav>
+    <form class="search">
+        <div class="input">
+            <input type="text" name="search">
+            <a title="Search by voice" class="tooltip"><span class="mic"></span></a>
         </div>
-    </div><!--.wrapper-->
+        <div class="buttons">
+            <a href="#" class="button grey">Google Search</a>
+            <a href="#" class="button grey">I'm Feeling Lucky</a>
+        </div>
+    </form>
+
+    <nav class="bottom-nav">
+        <ul>
+            <li><a href="#">Advertising Programs</a></li>
+            <li><a href="#">Business Solutions</a></li>
+            <li><a href="#">Privacy &amp; Terms</a></li>
+            <li><a href="#">&plus;Google</a></li>
+            <li><a href="#">About Google</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
